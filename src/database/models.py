c424a369257b7593535791382267860f95579108
faisal3397 <faisal.alowaisi.97@gmail.com>
2020-12-15 14:13:36 +0300

    print statement in models.py

--- src/database/models.py
+++ src/database/models.py
@@ -12,6 +12,7 @@ db = SQLAlchemy()
 
 def setup_db(app, database_path=database_path):
     app.config['SQLALCHEMY_DATABASE_URI'] = database_path if database_path else local_database_path
+    print('Database Path: {}'.format(app.config['SQLALCHEMY_DATABASE_URI']))
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
